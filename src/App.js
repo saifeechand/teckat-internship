aa4b32eaaaa5837fe2ef5374929b8128e31073d3
saifeechand <saifeechand3@gmail.com>
2024-06-05 18:39:12 +0530

    problem

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   // let str = "Hello"
@@ -10,27 +11,61 @@ function App() {
   //     console.log("hello world")
   //   }
 //   
-const student = {
-  student_name: "saifee",
+// const student = {
+//   student_name: "saifee",
+// }
+// const student2 = {
+//   student_name: "saifee",
+// }
 
-}
-const student2 = {
-  student_name: "saifee",
-}
+// const fun = () =>{
+//   console.log(1+5)
+// }
+// if(student.student_name === student2.student_name)
+//   {
+//     fun()
+//   }
+//   else{
+//     console.log("hello world")
+//   }
+const student = [
+  {name: "saifee", age:30},
+  {name: "saif", age:75},
+  {name: "imran", age:70},
+];
 
-const fun = () =>{
-  console.log(1+5)
-}
-if(student.student_name === student2.student_name)
-  {
-    fun()
-  }
-  else{
-    console.log("hello world")
-  }
+// for(let item of people){
+//   console.log(item.name)
+// };
 
+// people.forEach((person) =>
+//   {
+//     // console.log(person.name)
+// });
+// const employee = {
+//   name: "puja",
+//   age: 89,
+//   occupation: "hmmm....",
+// };
+// console.log(employee.name);
+ 
+ const employee = [
+   { name: "saifee", age: 22, salary: 55} ,
+   { name: "saif", age: 23, salary: 55},
+   { name: "imaran", age: 22, salary:55},
+ ];
+const mappedemployeea = employees.map((item) => {
+ console.log("item", item);
 
-    return (
+      return {
+        ...item,
+        salary: item.salary +100,
+      };
+    },[]);
+console.log("employees", employee);
+console.log("mappedEmployees",mappedEmployees);
+
+  return(
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
